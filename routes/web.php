80df292119edaad65c4451be00a3c3954f133170
KevinBrandao <kevenbrandaoms@outlook.com>
2023-03-14 22:53:17 -0300

    login e controller car

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+*Route::get('/', function () {
+*   return view('welcome');
+*});
+*/
+
+Route::get('/', [LoginController::class, 'login'])->name('login');
+Route::get('/register', [LoginController::class, 'register'])->name('register');
+Route::post('/signin', [LoginController::class, 'signin'])->name('signin');
+Route::post('/signup', [LoginController::class, 'signup'])->name('signup');
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::get('dashboard', function () {
+    return view('dashboard');
+})->name('dashboard')->middleware('auth');
